71548bb9515e696c8ab3925d1b06ea2b2c19d468
Nik <nik_palibrk@hotmail.com>
2024-08-08 17:36:36 +1000

    In JS created a a random number generator function to select cards from deck randomly. Originally I the card was created and appended in the click event listener, but I was having an issue with appending to the next avilable slot, due to the event being captured before my code could check if the slot was free?. I avoided adding any conditionals or logic in the event listener, therefore I created a function checkCardSlots to compare using an array to determine if the card slots were empty. There is no relation to the card slots on the field and the array, other than the index. The random number function is called here and then a card is appended via function to the parent element (p1cardSlot) and array value is changed to prevent appending more than once to this slot. Then I called the checkCardSlots function to the event listener. Note: it has come to my attention that I have doubled up some code here, such as in the for loop and in the createCardImage function. I could just return the cardSlotsValues array with the index parameter rather than creating an image element again. Lastly I have added JSdocs and labled my functions.

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
 const p1Avatar = document.querySelector('#p1Avatar');
-
 //Maybe I can select all and since they have an id in html file I may not have to do them indivdually like this?
 const p1CardSlot1 = document.querySelector('#p1CardSlot1');
 const p1CardSlot2 = document.querySelector('#p1CardSlot2');
 const p1CardSlot3 = document.querySelector('#p1CardSlot3');
 const p1CardSlot4 = document.querySelector('#p1CardSlot4');
 
+//Maybe add binary arrays?
+const cardSlotValues = [false, false, false, false];
+
 const cardImages = [];
 
-for(let i = 1; i <= 54; i++) {
-     const cardImg = document.createElement('img');
-     cardImg.className = 'cardImg';
-     cardImg.src = `Valheim_Images/Card_${i}.png`;
-     cardImages.push(cardImg);
+for (let i = 1; i <= 54; i++) {
+    const cardImg = document.createElement('img');
+    cardImg.className = 'cardImg';
+    cardImg.src = `Valheim_Images/Card_${i}.png`;
+    cardImages.push(cardImg);
 }
-p1CardSlot1.append(cardImages[0]);
-p1CardSlot2.append(cardImages[25]);
-p1CardSlot3.append(cardImages[40]);
-p1CardSlot4.append(cardImages[53]);
+/**
+ * Random number generator
+ * @returns Number
+ */
+function randNum() {
+    return Math.floor(Math.random() * 54) + 1;
+}
+/**
+ * Creates a card element using the cardImages array
+ * @param {Number} index 
+ * @returns Object
+ */
+function createCardImage(index) {
+    const cardImg = document.createElement('img');
+    cardImg.className = 'cardImg';
+    cardImg.src = `Valheim_Images/Card_${index}.png`;
+    return cardImg;
+}
+/**
+ * Function that checks if p1CardSlots are empty
+ * If empty appends to the first available card slot
+ */
+function checkCardSlots() {
+    let n = randNum();
+    if (cardSlotValues[0] === false) {
+        p1CardSlot1.append(createCardImage(n));
+        cardSlotValues[0] = true;
+
+    } else if (cardSlotValues[1] === false) {
+        p1CardSlot2.append(createCardImage(n));
+        cardSlotValues[1] = true;
+
+    } else if (cardSlotValues[2] === false) {
+        p1CardSlot3.append(createCardImage(n));
+        cardSlotValues[2] = true;
+
+    } else if (cardSlotValues[3] === false) {
+        p1CardSlot4.append(createCardImage(n));
+        cardSlotValues[3] = true;
+    }
+   
+}
+
+p1Avatar.addEventListener('click', () => {
+    checkCardSlots();
+});
